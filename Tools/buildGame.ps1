994bc6f510f01e272f4054fe2d4fc66b65289957
Andy Longhurst <andy@longhurst.net>
2024-07-25 08:39:54 +0100

    created DLL of sapia.game

--- Tools/buildGame.ps1
+++ Tools/buildGame.ps1
@@ -3,8 +3,8 @@ Set-Location Sapia.Game
 dotnet build -c Release
 dotnet test
 
-$src = "./Sapia.Game\bin\Release\netstandard2.1\Sapia.Logic.dll"
-$dest = "../Sapia/Assets"
+$src = "./Sapia.Game\bin\Release\netstandard2.1\Sapia.Game.dll"
+$dest = "../Sapia.Unity/Assets"
 
 Copy-Item  $src $dest
 Set-Location ..
