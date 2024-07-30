ed616848db40855e9d3564db80081ac6cc8f4c2f
Andy Longhurst <andy@longhurst.net>
2024-07-30 10:44:45 +0100

    sorted namespaces

--- Sapia.Game/Sapia.Game/Combat/Steps/MovedStep.cs
+++ Sapia.Game/Sapia.Game/Combat/Steps/MovedStep.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Sapia.Game.Combat.Entities;
+﻿using Sapia.Game.Combat.Entities;
 using Sapia.Game.Structs;
 
-namespace Sapia.Game.Combat.Steps
-{
-    public class MovedStep : CombatParticipantStep
-    {
-        public Coord PreviousPosition { get; }
+namespace Sapia.Game.Combat.Steps;
 
-        public MovedStep(Combat combat, CombatParticipant participant, Coord previousPosition) : base(combat, participant)
-        {
-            PreviousPosition = previousPosition;
-        }
+public class MovedStep : CombatParticipantStep
+{
+    public Coord PreviousPosition { get; }
+
+    public MovedStep(Combat combat, CombatParticipant participant, Coord previousPosition) : base(combat, participant)
+    {
+        PreviousPosition = previousPosition;
     }
 }
