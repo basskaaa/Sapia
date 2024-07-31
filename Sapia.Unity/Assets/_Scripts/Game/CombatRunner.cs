49439f65e29082e543ecbc12198bd5a702eb3be0
Andy Longhurst <andy@longhurst.net>
2024-07-31 14:42:16 +0100

    updated

--- Sapia.Unity/Assets/_Scripts/Game/CombatRunner.cs
+++ Sapia.Unity/Assets/_Scripts/Game/CombatRunner.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Assets._Scripts.TypeData;
+using Assets._Scripts.Ui;
 using Assets._Scripts.Ui.Test;
 using Sapia.Game.Characters;
 using Sapia.Game.Characters.Configuration;
@@ -31,45 +32,9 @@ namespace Assets._Scripts.Game
 
         private Combat CreateCombat()
         {
-            var theRockConfiguration = new CharacterConfiguration
-            {
-                Name = "Dwayne 'The Rock' Johnson",
-                LevelConfigurations = new()
-                {
-                    {1, new()
-                    {
-                        ClassId = "Fighter"
-                    }},
-                    {2, new()
-                    {
-                        ClassId = "Fighter"
-                    }}
-                }
-            };
+            var participantRefs = FindObjectsByType<CombatParticipantRef>(FindObjectsInactive.Include, FindObjectsSortMode.None);
 
-            var typeData = TypeDataFactory.CreateTypeData();
-            var characterStatusService = new CharacterService(typeData);
-
-            var theRock = characterStatusService.CompileCharacter(theRockConfiguration, new[] { "Jab", "Slash" });
-
-            ICompiledCharacter CreateSkeleton() =>  new SimpleCharacter("Skeleton", new CharacterStats(3))
-            {
-                Abilities = new[] { new PreparedAbility("Slash") }
-            };
-
-            var participantRefs = FindObjectsByType<CombatParticipantRef>( FindObjectsInactive.Include, FindObjectsSortMode.None);
-
-            var participants = participantRefs.Select(x =>
-            {
-                var pos = new Coord((int)x.transform.position.x, (int)x.transform.position.z);
-                var id = x.ParticipantId;
-
-                var character = x.ParticipantId == "Player" ? theRock : CreateSkeleton();
-
-                return new CombatFactory.CombatParticipantEntry(id, character, id == "Player" ? 20 : 5, pos);
-            });
-
-            var combat = CombatFactory.Create(typeData, participants);
+            var combat = CombatStarter.CreateCombat(participantRefs);
 
             foreach (var combatParticipantRef in participantRefs)
             {
@@ -81,6 +46,8 @@ namespace Assets._Scripts.Game
 
         public void Step()
         {
+            _combat.ExecuteAi();
+
             if (_combat.Step())
             {
                 ShowDebugText();
@@ -90,91 +57,10 @@ namespace Assets._Scripts.Game
                 ShowDebugText("Combat finished");
             }
 
-            RaiseStepChanged();
-        }
-
-        private void RaiseStepChanged()
-        {
             foreach (var combatListener in _listeners)
             {
                 combatListener.StepChanged(_combat, _combat.CurrentStep);
             }
-        }
-
-        public void Act()
-        {
-            AutoRunStep();
-        }
-
-        private void AutoRunStep()
-        {
-            if (_combat == null || _combat.CurrentStep == null)
-            {
-                return;
-            }
-
-            var turn = _combat.CurrentStep as TurnStep;
-
-            if (turn == null)
-            {
-                return;
-            }
-
-            if (turn.Abilities.Count == 0)
-            {
-                turn.EndTurn();
-                ShowDebugText("Ended turn", "Use Step to advance combat");
-                RaiseStepChanged();
-                return;
-            }
-
-            CombatParticipant FindTargetFor(CombatParticipant participant)
-            {
-                foreach (var other in _combat.Participants.All)
-                {
-                    if (other.Character.IsPlayer != participant.Character.IsPlayer && other.Character.IsAlive)
-                    {
-                        return other;
-                    }
-                }
-
-                return null;
-            }
-
-            var ability = turn.Abilities.Last();
-
-            var target = FindTargetFor(turn.Participant);
-
-            if (target != null)
-            {
-                var abilityId = ability.AbilityType.Id;
-                var targetParticipantId = target.ParticipantId;
-
-                UseAbilityInCurrentTurn(turn, abilityId, targetParticipantId);
-            }
-            else
-            {
-                ShowDebugText($"Unable to find a target for {ability.AbilityType.Id}");
-            }
-        }
-
-        private void UseAbilityInCurrentTurn(TurnStep turn, string abilityId, string targetParticipantId)
-        {
-            var result = turn.UseAbility(new TargetedAbilityUse(abilityId, targetParticipantId));
-
-            _combat.Step();
-
-            if (result.HasValue)
-            {
-                var targetInfo = result.Value.AffectedParticipants.Select(x => $"{x.ParticipantId} {x.HealthChange}").ToArray();
-                ShowDebugText($"Used {result.Value.Ability.Id}: {string.Join(", ", targetInfo)}");
-            }
-            else
-            {
-                ShowDebugText($"Failed to use {abilityId}");
-            }
-
-            RaiseStepChanged();
         }
 
         public bool Move(string participantId, Coord coord)
@@ -202,7 +88,14 @@ namespace Assets._Scripts.Game
                 UseAbilityInCurrentTurn(turn, ability.AbilityType.Id, targetParticipantId);
             }
         }
-        
+
+        private void UseAbilityInCurrentTurn(TurnStep turn, string abilityId, string targetParticipantId)
+        {
+            turn.UseAbility(new TargetedAbilityUse(abilityId, targetParticipantId));
+
+            Step();
+        }
+
         private void ShowDebugText(params string[] text)
         {
             var textToShow = new List<string>();
---
+++ Sapia.Unity/Assets/_Scripts/Game/CombatStarter.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Linq;
+using Assets._Scripts.Game;
+using Assets._Scripts.TypeData;
+using Sapia.Game.Characters;
+using Sapia.Game.Characters.Configuration;
+using Sapia.Game.Combat;
+using Sapia.Game.Structs;
+using UnityEngine;
+
+public static class CombatStarter
+{
+    public static Combat CreateCombat(IReadOnlyCollection<CombatParticipantRef> participantRefs)
+    {
+        var theRockConfiguration = new CharacterConfiguration
+        {
+            Name = "Dwayne 'The Rock' Johnson",
+            LevelConfigurations = new()
+            {
+                {1, new()
+                {
+                    ClassId = "Fighter"
+                }},
+                {2, new()
+                {
+                    ClassId = "Fighter"
+                }}
+            }
+        };
+
+        var typeData = TypeDataFactory.CreateTypeData();
+        var characterStatusService = new CharacterService(typeData);
+
+        var theRock = characterStatusService.CompileCharacter(theRockConfiguration, new[] { "Jab", "Slash" });
+
+        ICompiledCharacter CreateSkeleton() => new SimpleCharacter("Skeleton", new CharacterStats(3))
+        {
+            Abilities = new[] { new PreparedAbility("Slash") }
+        };
+
+        var participants = participantRefs.Select(x =>
+        {
+            var pos = new Coord((int)x.transform.position.x, (int)x.transform.position.z);
+            var id = x.ParticipantId;
+
+            var character = x.ParticipantId == "Player" ? theRock : CreateSkeleton();
+
+            return new CombatFactory.CombatParticipantEntry(id, character, id == "Player" ? 20 : 5, pos);
+        });
+
+        var combat = CombatFactory.Create(typeData, participants);
+
+        return combat;
+    }
+}
